81c666242816854c997f5a31a2cf8c8f43ca4800
Samarjeet <singh.samarjeet.27@gmail.com>
2015-01-18 16:52:24 +0530

    Load calendar only when required

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 // main script
 
 $(document).ready(function () {
-	$(".responsive-calendar").responsiveCalendar({
-	time: '2015-01',
-	events: {
-		"2015-01-30": {"number": 5, "url": "event no. 5"},
-		"2015-01-26": {"number": 1, "url": "event no. 1"}, 
-		"2015-01-03":{"number": 1}, 
-		"2015-01-12": {}}
-	});
+
+	if($('.responsive-calendar').length  != 0) {
+			$(".responsive-calendar").responsiveCalendar({
+				time: '2015-01',
+				events: {
+					"2015-01-30": {"number": 5, "url": "event no. 5"},
+					"2015-01-26": {"number": 1, "url": "event no. 1"}, 
+					"2015-01-03":{"number": 1}, 
+					"2015-01-12": {}}
+				}
+		);
+	}
 });
